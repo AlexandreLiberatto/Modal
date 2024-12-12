a20142af10d5d04159e561611098b98e37db9ba4
Alexandre liberato <alexandreliberatto@gmail.com>
2024-12-12 17:15:45 -0300

    Manipulando o DOM, criando variáveis no arquivo .js
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const modal = document.querySelector('.modal');
+const modalBase = document.querySelector('.modal-base');
+const modalConteudo = document.querySelector('.modal-conteudo');
+const btnModal = document.querySelector('.btn-modal');
+const btnFechar = document.querySelector('.btn-fechar');
