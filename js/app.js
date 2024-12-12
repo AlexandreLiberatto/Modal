0d7ac8326d9279f2eba5702cc3cd9c0df9c28646
Alexandre liberato <alexandreliberatto@gmail.com>
2024-12-12 18:34:52 -0300

    Criando funções para abrir e fechar o modal
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

--- js/app.js
+++ js/app.js
@@ -3,3 +3,13 @@ const modalBase = document.querySelector('.modal-base');
 const modalConteudo = document.querySelector('.modal-conteudo');
 const btnModal = document.querySelector('.btn-modal');
 const btnFechar = document.querySelector('.btn-fechar');
+
+btnModal.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    modalBase.classList.add('slide');
+    modalConteudo.scrollTop = 0;
+});
+
+btnFechar.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
